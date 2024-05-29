853d1ad0d930d36d69db63634da054d813147a3d
dusklight00 <r.rahul.developer@gmail.com>
2024-05-30 02:56:47 +0530

    login page complete

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,38 +1,11 @@
 import Webcam from "react-webcam";
-import {
-  Container,
-  Card,
-  CardContent,
-  Typography,
-  TextField,
-  Button,
-  Stack,
-} from "@mui/material";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
-    <Container className="h-[100vh] flex place-content-center" maxWidth="sm">
-      <Card>
-        <CardContent>
-          <Stack gap={3}>
-            <Typography variant="h5">
-              <b>Login</b>
-            </Typography>
-            <TextField
-              label="Username"
-              variant="outlined"
-              // onChange={(e) => setUsername(e.target.value)}
-            />
-            <TextField
-              label="Password"
-              variant="outlined"
-              // onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button variant="contained">Login</Button>
-          </Stack>
-        </CardContent>
-      </Card>
-    </Container>
+    <>
+      <Login />
+    </>
   );
 }
 
--- view/src/pages/Login.jsx
+++ view/src/pages/Login.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import {
+  Container,
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+  Stack,
+} from "@mui/material";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Container className="h-[100vh] flex place-content-center" maxWidth="sm">
+      <Card>
+        <CardContent>
+          <Stack gap={3}>
+            <Typography variant="h5">
+              <b>Login</b>
+            </Typography>
+            <TextField
+              label="Username"
+              variant="outlined"
+              // onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              label="Password"
+              variant="outlined"
+              // onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button variant="contained">Login</Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 }
 
 export default Login;
